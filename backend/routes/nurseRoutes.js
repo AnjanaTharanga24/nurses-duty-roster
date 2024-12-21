b1c0957a487fae517f24fa1589eb4aa81a7b8e93
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-12-22 04:50:53 +0530

    new update

--- backend/routes/nurseRoutes.js
+++ backend/routes/nurseRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const Nurse = require('../models/nurseModel');
-const { createNurse, getNurseById } = require('../controllers/nurseController');
+const { createNurse, getNurseById, getAllNurses } = require('../controllers/nurseController');
 const router = express.Router();
 
 router.route("/create").post(createNurse);
+router.route("/").get(getAllNurses);
 router.route("/:id").get(getNurseById);
 
 module.exports = router;
--- frontend/src/component/DisplayNurses.jsx
+++ frontend/src/component/DisplayNurses.jsx
@@ -1,36 +1,49 @@
 import React, { useEffect, useState } from "react";
-import logo from '../assets/images/logo.png'
+import logo from "../assets/images/logo.png";
 import axios from "axios";
 function DisplayNurses() {
-  const [nurses,setNurses] = useState([]);
+  const [nurses, setNurses] = useState([]);
 
-  useEffect(()=>{
-    getAllNurses()
-  },[])
+  useEffect(() => {
+    getAllNurses();
+  }, []);
 
-  const getAllNurses = async () =>{
-        try {
-          const response = await axios.get("http://localhost:3000/api/nurse/")
-          setNurses(response.data)
-          console.log(response.data)
-        } catch (error) {
-          console.log({msg:error.message})
-        }
-  }
+  const getAllNurses = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/nurse/");
+      setNurses(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log({ msg: error.message });
+    }
+  };
   return (
     <div className="container">
       <div className="row">
-        {nurses.map((nurse)=> (
-           <div className="col-md-4" key={nurse._id}>
-             <div className="card nurse-card shadow">
-              <img src={logo} width="100px" className="card-img mt-3" alt="nurse logo"/>
+        {nurses.map((nurse) => (
+          <div className="col-md-4" key={nurse._id}>
+            <div className="card nurse-card shadow">
+              <img
+                src={logo}
+                width="100px"
+                className="card-img mt-3"
+                alt="nurse logo"
+              />
               <h2 className="card-title text-center mt-3">{nurse.name}</h2>
-            </div>
-           </div>
-        ))}
-        
 
-       </div>
+              <select
+                className="form-select w-75 align-self-center mt-3 mb-3"
+                aria-label="Default select example"
+              >
+                <option selected="">Select Duty Type</option>
+                <option value={1}>Morning</option>
+                <option value={2}>Afternoon</option>
+                <option value={3}>Night</option>
+              </select>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
