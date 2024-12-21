d13f1d3c22027527fcfdec3e06e53d5c6478654c
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-12-22 03:37:38 +0530

    create display nurses component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 import Home from './pages/Home';
+import { ToastContainer } from 'react-toastify';
 
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <ToastContainer/>
     </div>
   );
 }
--- frontend/src/component/CreateNurseForm.jsx
+++ frontend/src/component/CreateNurseForm.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { toast, ToastContainer } from 'react-toastify';
 
-function CreateNurseForm() {
-  const handleSubmit = (e) => {
+
+function CreateNurseForm({onClose}) {
+
+  const [formData,setFormData] = useState({
+     name:""
+  })
+  const {name} = formData
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Add your form submission logic here
+    try {
+      const response = await axios.post("http://localhost:3000/api/nurse/create",formData);
+
+      setFormData(response.data);
+      console.log(response.data);
+      onClose();
+
+      toast.success('Nurse created successfully!', {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
+      
+      setTimeout(()=>{
+        window.location.reload();
+      },3500)
+      
+    } catch (error) {
+      toast.error('please add a name', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
+
+      console.log({msg:error.message})
+    }
+    
   };
 
   return (
@@ -13,11 +54,14 @@ function CreateNurseForm() {
         <input
           type="text"
           className="form-control"
+          name="name"
+          value={name}
+          onChange={(e)=>setFormData({...formData,name:e.target.value})}
           id="nurseName"
           placeholder="Enter name"
         />
       </div>
-      <div className="text-end">
+      <div>
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
